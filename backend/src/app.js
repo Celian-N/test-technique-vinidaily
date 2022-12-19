bbb441edf5732450e77d61f9ec188a05cd20c83e
Celian-N <celian.niclot@gmail.com>
2022-12-19 16:39:31 +0100

    :sparkles: create server

---
+++ backend/src/app.js
@@ -0,0 +1,29 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const cors = require("cors");
+
+app.use(cors());
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
