b5acddacebfd225523eed1538a837dc1dead92fb
Celian-N <celian.niclot@gmail.com>
2022-12-19 17:02:17 +0100

    add Wine interface

---
+++ frontend/src/types/wines.ts
@@ -0,0 +1,19 @@
+export interface Wine {
+  millesimed: boolean;
+  year: number;
+  grapeVarieties: string;
+  logo: string;
+  size: number;
+  region: string;
+  agingPotential: number;
+  name: string;
+  description: string;
+  type: string;
+  cuve: string;
+  cru: string;
+  appelation: string;
+  appelationType: string;
+  price: number;
+  note: number;
+  winePaired: string[];
+}
